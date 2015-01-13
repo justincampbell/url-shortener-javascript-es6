1d96741db153acf2f1863805753892a3809d29fa
Justin Campbell <justin@justincampbell.me>
2015-01-12 21:59:12 -0500

    Create /shorten route

--- test/web_test.js
+++ test/web_test.js
@@ -2,12 +2,34 @@ var assert = require('assert');
 var request = require('supertest');
 
 var app = require('../web').app;
+var url = 'http://justincampbell.me';
 
-describe('GET /', function(){
-  it('redirects to the home page', function(done){
+describe('GET /', function() {
+  it('redirects to the home page', function(done) {
     request(app)
       .get('/')
       .expect('Location', 'https://github.com/justincampbell/url-shorteners')
       .expect(302, done);
-  })
+  });
+});
+
+describe('GET /shorten', function() {
+  it('returns a token path when given a url', function(done) {
+    request(app)
+      .get('/shorten?url=' + url)
+      .expect(/\/\d+/)
+      .expect(201, done);
+  });
+
+  it('returns bad request when the url is missing', function(done) {
+    request(app)
+      .get('/shorten')
+      .expect(400, done);
+  });
+
+  it('returns bad request when the url is blank', function(done) {
+    request(app)
+      .get('/shorten?url=')
+      .expect(400, done);
+  });
 });
--- web.js
+++ web.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var shortener = require('./shortener');
 
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
   res.redirect('https://github.com/justincampbell/url-shorteners');
 });
+
+app.get('/shorten', function (req, res, next) {
+  var url = req.query.url;
+
+  if (!url) { return res.sendStatus(400); }
+
+  var token = shortener.shorten(url);
+  res.status(201).send('/' + token);
+});
+
 
 app.listen(process.env.PORT || 3000);
 
